b0068829b5c90645f88b2c677bc0bf8371330931
Matthew A Schultz <matthewschultz@me.com>
2022-10-21 23:51:32 -0600

    Initial groundwork for entity stimulus system, not quite there yet.

---
+++ src/EntityNode2D.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+enum Source {TREE}
+
+var source : int # Override on child
+
+signal clicked(area)
+signal entered(area)
+signal exited(area)
+
+
+func _stimulus_clicked(area: Area2D) -> void:
+    print("Clicked, Source: ", area.source, "Stimulus: ", area.stimulus)
+
+
+func _stimulus_entered(area: Area2D) -> void:
+    print("Entered, Source: ", area.source, "Stimulus: ", area.stimulus)
+
+
+func _stimulus_exited(area: Area2D) -> void:
+    print("Exited, Source: ", area.source, "Stimulus: ", area.stimulus)
