e97cc32070feb58bf73fdcc4ed583e9da15fdc1b
Matthew A Schultz <matthewschultz@me.com>
2022-10-22 12:25:42 -0600

    Signals for interacted and stimuluses entering and exiting

--- src/entities/Entity.gd
+++ src/entities/Entity.gd
@@ -7,6 +7,10 @@ var _state : int
 var _state_entered : bool
 var _state_exited : bool
 
+signal interacted
+signal stimulus_entered(area)
+signal stimulus_exited(area)
+
 func _ready() -> void:
     pass
 
@@ -27,12 +31,15 @@ func _process(_delta: float):
 
 func clicked() -> void:
     print("[ENTITY] %s clicked" % [name])
+    emit_signal("interacted")
 
 func stimulus_entered(area: EntityArea2D) -> void:
     print("[ENTITY] %s entered | source: %s | stimulus: %s" % [name, area.source, area.stimulus])
+    emit_signal("stimulus_entered", area)
 
 func stimulus_exited(area: EntityArea2D) -> void:
     print("[ENTITY] %s exited | source: %s | stimulus: %s" % [name, area.source, area.stimulus])
+    emit_signal("stimulus_exit", area)
 
 func change_state(state: int) -> void:
     print("[ENTITY] %s changing state: %s" % [name, Enums.EntityStates.keys()[state]])
