3f4a13748eb64c595e4a1997805c00869873c6b0
Colin Bellino <bellino.colin@gmail.com>
2022-10-23 04:18:14 +0200

    Fix input map so it works on touchscreen too

--- src/MouseArea2D.gd
+++ src/MouseArea2D.gd
@@ -3,11 +3,11 @@ class_name MouseArea2D extends Area2D
 onready var entity = get_parent()
 
 func _ready() -> void:
-    var _result = connect("input_event", self, "_mouse_event")
+    var _result = connect("input_event", self, "on_input_event")
 
-func _mouse_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+func on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
     if event is InputEventMouseMotion:
         pass # We could do something with mouseovers here
 
-    if event is InputEventMouseButton and event.is_action_pressed("mouse_left"):
+    if event.is_action_released("mouse_left"):
         entity.interact()
