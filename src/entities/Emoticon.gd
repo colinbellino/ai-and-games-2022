e75a74b046bd93f89eb59b993f5fcc99b821e15b
Matthew A Schultz <matthewschultz@me.com>
2022-10-23 16:14:41 -0600

    Initial emoticon stuff

---
+++ src/entities/Emoticon.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+tool
+
+enum {MUSIC, SAD, WATER, CIRCLE, STARS, HAHA, HAPPY, DOTDOTDOT, MONEY, STAR, LIGHTBULB, BLANK, DOTDOT, RAIN, ZZ, HEARTS, DOUBLEEXCLIMATION, DOT, ?, Z, BROKENHEART, EXCLIMATION, NO, SUPRISE}
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#    pass
