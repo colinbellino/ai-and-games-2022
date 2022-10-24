c36b79aef98dfc26308a43fe8a78f05bfb6d7ee6
Colin Bellino <bellino.colin@gmail.com>
2022-10-24 15:19:40 +0200

    Fix indent

--- src/behaviours/Hunger.gd
+++ src/behaviours/Hunger.gd
@@ -7,30 +7,28 @@ const EMOTION_TICKS = 5 # Check for emotional impact every
 const EMOTIONAL_THRESHOLD = 10 # Threshold for emotional impact
 
 func _ready() -> void:
-	Globals.hunger = STARTING_HUNGER
+    Globals.hunger = STARTING_HUNGER
 
-	var hunger_timer = Timer.new()
-	add_child(hunger_timer)
-	hunger_timer.connect("timeout", self, "_hunger_timeout")
-	hunger_timer.one_shot = false # loop timer
-	hunger_timer.start(HUNGER_TICKS)
+    var hunger_timer = Timer.new()
+    add_child(hunger_timer)
+    hunger_timer.connect("timeout", self, "_hunger_timeout")
+    hunger_timer.one_shot = false # loop timer
+    hunger_timer.start(HUNGER_TICKS)
 
-	# We might want something more global where we have
-	# a number that changes based on factors that
-	# will hit emotion at specific intrivals
-	var emotional_impact_timer = Timer.new()
-	add_child(emotional_impact_timer)
-	emotional_impact_timer.connect("timeout", self, "_emotional_impact_timeout")
-	emotional_impact_timer.one_shot = false # loop timer
-	emotional_impact_timer.start(EMOTION_TICKS)
-
+    # We might want something more global where we have
+    # a number that changes based on factors that
+    # will hit emotion at specific intrivals
+    var emotional_impact_timer = Timer.new()
+    add_child(emotional_impact_timer)
+    emotional_impact_timer.connect("timeout", self, "_emotional_impact_timeout")
+    emotional_impact_timer.one_shot = false # loop timer
+    emotional_impact_timer.start(EMOTION_TICKS)
 
 func _hunger_timeout():
-	Globals.hunger -= 1
-	print("[HUNGER] ticked: ", Globals.hunger)
-
+    Globals.hunger -= 1
+    print("[HUNGER] ticked: ", Globals.hunger)
 
 func _emotional_impact_timeout():
-	if Globals.hunger <= EMOTIONAL_THRESHOLD:
-		Globals.emotion_level -= 1
-		print("[HUNGER] emotional impact: ", Globals.emotion_level)
+    if Globals.hunger <= EMOTIONAL_THRESHOLD:
+        Globals.emotion_level -= 1
+        print("[HUNGER] emotional impact: ", Globals.emotion_level)
