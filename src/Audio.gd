4c3bf964e08e43690eff1f0c0ec70bb00e1158c1
Colin Bellino <bellino.colin@gmail.com>
2022-10-25 02:20:23 +0200

    Creature now wakes up again

--- src/Audio.gd
+++ src/Audio.gd
@@ -8,13 +8,13 @@ static func play_sound(id: int, position: Vector3 = Vector3.ZERO, loop: bool = f
     Globals.audio_player_sound.stream = stream
     Globals.audio_player_sound.play()
 
-static func stop_sound() -> void:
-    Globals.audio_player_sound.stop()
-
 static func play_sound_random(ids: PoolIntArray, position: Vector3 = Vector3.ZERO, loop: bool = false) -> void:
     var id := randi() % ids.size()
     assert(Globals.audio_sounds.has(id), "[Audio] Stream not found (sound_random): %s" % [id])
     play_sound(id, position, loop)
+
+static func stop_sound() -> void:
+    Globals.audio_player_sound.stop()
 
 static func play_music(id: int, loop: bool = true) -> void:
     assert(Globals.audio_sounds.has(id), "[Audio] Stream not found (music): %s" % [id])
