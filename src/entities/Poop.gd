db405d7a5a4e906d98367769a59594b5ca53d804
Matthew A Schultz <matthewschultz@me.com>
2022-10-26 15:37:54 -0600

    Cleaning poop positively impacts emotion

--- src/entities/Poop.gd
+++ src/entities/Poop.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+const CLEAN_POOP_MOD = Vector2(-0.1, -0.2)
+
+
 func _ready() -> void:
     add_to_group("poops")
     Audio.play_sound(Globals.SFX.POOP)
 
 func interact(_interaction_type: int) -> void:
+    Globals.add_emotion(CLEAN_POOP_MOD, "Clean poop")
     queue_free()
