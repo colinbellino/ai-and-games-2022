a3f49a88278337166d07350f7f383c625c1e1427
Matthew A Schultz <matthewschultz@me.com>
2022-10-25 18:27:58 -0600

    Comparsions for emotion using vect2's

--- src/behaviours/Bark.gd
+++ src/behaviours/Bark.gd
@@ -2,7 +2,9 @@ class_name Bark extends Behaviour
 
 const BARK_DELAY_MIN_IN_MS : int = 2000
 const BARK_DELAY_MAX_IN_MS : int = 8000
-const EMOTION_DEATH_THRESHOLD : int = -20
+const EMOTION_DEATH_THRESHOLD : int = -1
+const BORED_THRESHOLD := Vector2(-0.5,-0.5)
+const HAPPY_THRESHOLD := Vector2(0.5,0.5)
 
 var next_bark : int
 
@@ -16,15 +18,14 @@ func _exit_tree() -> void:
 func _process(_delta: float) -> void:
     if entity._state == Enums.EntityStates.Idle:
         if Globals.time_elapsed > next_bark:
-            if Globals.emotion < EMOTION_DEATH_THRESHOLD:
+            if Globals.emotion.y < EMOTION_DEATH_THRESHOLD:
                 entity.set_meta("dead_animation", "dead_leave")
                 entity.change_state(Enums.EntityStates.Dead)
-            # Live, laugh, love very inspirational
-            elif Globals.emotion < -5: # Sad
+            elif Globals.emotion.x <= BORED_THRESHOLD.x and Globals.emotion.y <= BORED_THRESHOLD.y: # Sad
                 entity.set_meta("bark_animation", "cry")
                 entity.change_state(Enums.EntityStates.Bark)
                 Audio.play_sound(Globals.SFX.CRY)
-            elif Globals.emotion > 5: # Happy
+            elif Globals.emotion.x >= HAPPY_THRESHOLD.x and Globals.emotion.y >= HAPPY_THRESHOLD.y: # Happy
                 entity.set_meta("bark_animation", "laugh")
                 entity.change_state(Enums.EntityStates.Bark)
             else: # Neutral
