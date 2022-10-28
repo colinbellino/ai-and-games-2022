ba67109805178e1046a6112808ddda0ba430f8e9
Colin Bellino <bellino.colin@gmail.com>
2022-10-28 12:07:10 +0200

    Feed button cooldown

--- src/PlayUI.gd
+++ src/PlayUI.gd
@@ -5,6 +5,8 @@ var label_name: Label
 var hunger_progress: TextureProgress
 var settings_button: TextureButton
 var feed_button: Button
+var feed_sprite: Sprite
+var next_feed : float
 
 func _ready() -> void:
     container_control = get_node("%Container")
@@ -12,6 +14,7 @@ func _ready() -> void:
     hunger_progress = get_node("%HungerProgress")
     settings_button = get_node("%SettingsButton")
     feed_button = get_node("%FeedButton")
+    feed_sprite = get_node("%FeedSprite")
 
     settings_button.connect("pressed", self, "settings_button_pressed")
     feed_button.connect("pressed", self, "feed_button_pressed")
@@ -19,6 +22,11 @@ func _ready() -> void:
     feed_button.connect("mouse_exited", self, "feed_button_mouse_exited")
 
     close()
+
+func _process(_delta: float) -> void:
+    feed_button.disabled = Globals.time_elapsed < next_feed
+    feed_sprite.modulate.a = 0.3 if Globals.time_elapsed < next_feed else 1.0
+    hunger_progress.value = Globals.hunger / float(Globals.HUNGER_MAX) * 100.0
 
 func open() -> void:
     label_name.text = Globals.creature_name
@@ -35,17 +43,17 @@ func close() -> void:
 
     visible = false
 
-func _process(_delta: float) -> void:
-    hunger_progress.value = Globals.hunger / float(Globals.HUNGER_MAX) * 100.0
-
 func settings_button_pressed() -> void:
     Globals.ui_settings.open(true)
     Audio.play_sound_random([Globals.SFX.BUTTON_CLICK_2])
 
 func feed_button_pressed() -> void:
-    var amount : int = LDTK.get_behaviour_meta(Globals.creature, "FoodSource", "Amount", 20)
-    Audio.play_sound(Globals.SFX.BUTTON_CLICK_1, Globals.creature.position)
-    Globals.creature.emit_signal("fed", amount)
+    if Globals.time_elapsed >= next_feed:
+        var amount : int = LDTK.get_behaviour_meta(Globals.creature, "FoodSource", "Amount", 30)
+        Audio.play_sound(Globals.SFX.BUTTON_CLICK_1, Globals.creature.position)
+        Globals.creature.emit_signal("fed", amount)
+        next_feed = Globals.time_elapsed + 5000.0
+        Globals.set_cursor(Globals.CURSORS.DEFAULT)
 
 func feed_button_mouse_entered() -> void:
     Globals.set_cursor(Globals.CURSORS.HAND)
