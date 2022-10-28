7d25f72c872bea8f7e1e3f5045961d170adfb2e8
BitBionic <brandon.forrester@gmail.com>
2022-10-27 21:52:42 -0400

    Added dino shadow..

--- src/behaviours/Shadow.gd
+++ src/behaviours/Shadow.gd
@@ -1,4 +1,11 @@
 class_name Shadow extends Behaviour
 
+var shadow_scn = ResourceLoader.load("res://media/scenes/entities/Shadow.tscn")
+
 func _ready() -> void:
-    pass
+    yield(get_tree(), "idle_frame")
+    var shadow = shadow_scn.instance()
+    entity.add_child(shadow)
+    entity.move_child(shadow, 1)
+    # Globals.entities_node.move_child(poop_fx, 1)
+
